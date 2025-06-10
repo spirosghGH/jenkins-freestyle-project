c1e097b0e1d0499805f7a5444f2acbf839661fc5
spirosghGH <spirosgh1995@gmail.com>
2025-06-10 10:38:49 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 echo 'Hello from run.sh'
 echo 'This is being run from a jenkins job'
-echo 'This is a retest of the job with no changes'
+echo 'This is a re-test of the job with no changes'
+echo 'The end of the test'
